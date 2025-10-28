7220d8c7c16139eaaa9b558d093ca19ee9ea0405
yozCode <fatevorwa@gmail.com>
2025-10-28 14:33:16 +0700

    Update B.c

--- Week 5 - Struct/B.c
+++ Week 5 - Struct/B.c
@@ -19,24 +19,29 @@ int main() {
         for (int i = 0; i < N; i++) {
             scanf("%s %d", students[i].name, &students[i].group);
         }
-        printf ("Case #%d:\n");
-        for (int g = 1; g <= 20; g++){
-        	int count = 0;
-        	 for (int i = 0; i < N; i++) {
-            if (students[i].group == g){
-        		count ++;
-			}
+        
+        printf("Case #%d:\n", tc);
+        
+
+        for (int g = 1; g <= 20; g++) {
+            int count = 0;
+            for (int i = 0; i < N; i++) {
+                if (students[i].group == g) {
+                    count++;
+                }
+            }
+            
+            if (count > 0) {
+                printf("Group %d(%d):\n", g, count);
+                
+                for (int i = 0; i < N; i++) {
+                    if (students[i].group == g) {
+                        printf("%s\n", students[i].name);
+                    }
+                }
+            }
         }
-			if (count > 0){
-			printf ("Group %d(%d):\n", g, count);{
-			for (int i = 0; i < N; i++){
-			 if (students[i].group == g){
-			printf ("%s\n", students[i].name);
-			 }
-			}
-			}
-		}
-		}
-	}
+    }
+    
     return 0;
 }
