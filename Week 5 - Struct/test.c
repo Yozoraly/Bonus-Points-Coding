b0c9e541be59a2ed2fd304f0d554a49fa80319cb
yozCode <fatevorwa@gmail.com>
2025-10-28 14:38:15 +0700

    REVISI A DAN D

---
+++ Week 5 - Struct/test.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+
+struct Student {
+    char id[25];
+    char name[105];
+    int age;
+};
+
+int main() {
+    int N;
+    scanf("%d", &N);
+    getchar();
+
+    struct Student students[1005];
+
+    for (int i = 0; i < N; i++) {
+        fgets(students[i].id, sizeof(students[i].id), stdin);
+        students[i].id[strcspn(students[i].id, "\n")] = '\0'; 
+
+        fgets(students[i].name, sizeof(students[i].name), stdin);
+        students[i].name[strcspn(students[i].name, "\n")] = '\0'; 
+
+        scanf("%d", &students[i].age);
+        getchar(); 
+    }
+
+    int Q;
+    scanf("%d", &Q);
+
+    for (int i = 1; i <= Q; i++) {
+        int X;
+        scanf("%d", &X);
+        printf("Query #%d:\n", i);
+        printf("ID: %s\n", students[X - 1].id);
+        printf("Name: %s\n", students[X - 1].name);
+        printf("Age: %d\n", students[X - 1].age);
+    }
+
+    return 0;
+}
+
